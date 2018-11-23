19177d507fcbd65a5e6fe90fb0d6058a64c1822f
Anthony Ko <anthonyko@Anthonys-MacBook-Pro.local>
2018-11-23 12:30:50 -0800

    changes

--- app.py
+++ app.py
@@ -4,60 +4,83 @@ import json
 import os
 import xml.etree.ElementTree as ET
 import datetime
+
 app = Flask(__name__)
-url = "https://api.hfs.purdue.edu/menus/v2/locations/"
-locations = ["Wiley", "Ford", "Hillenbrand", "The Gathering Place", "Earhart", "Windsor"]
+
+# Constants
+URL = "https://api.hfs.purdue.edu/menus/v2/locations/"
+locations = [
+    "Wiley",
+    "Ford",
+    "Hillenbrand",
+    "The Gathering Place",
+    "Earhart",
+    "Windsor",
+]
+
+# get messenger api tokens specified by env variables
 verify_token = os.getenv("VERIFY_TOKEN")
-ti1 = datetime.datetime.now()
-time = ti1.strftime("%m-%d-%Y")
 access_token = os.getenv("ACCESS_TOKEN")
 
-@app.route("/",methods=['GET','POST'])
+ti1 = datetime.datetime.now()
+time = ti1.strftime("%m-%d-%Y")
+
+
+@app.route("/", methods=["GET", "POST"])
 def default():
     return "def"
 
-@app.route("/webhook",methods=['GET'])
+
+@app.route("/webhook", methods=["GET"])
 def verify():
-    if request.args.get('hub.verify_token') == verify_token:
-        return request.args.get('hub.challenge')
+    if request.args.get("hub.verify_token") == verify_token:
+        return request.args.get("hub.challenge")
     return "Wrong verify token"
 
-@app.route("/webhook",methods=['POST'])
+
+@app.route("/webhook", methods=["POST"])
 def webhook():
     data = json.loads(request.data)
     entries = data["entry"]
     if data["object"] == "page":
         returnmessage = ""
         for entry in entries:
-            m= entry.get('messaging')
-            if(m==None):
+            m = entry.get("messaging")
+            if m == None:
                 return "no message found"
-            userm = m[0].get('message')
-            if(userm==None):
+            userm = m[0].get("message")
+            if userm == None:
                 return "no message found"
-            useri = m[0].get('sender')
-            if(useri==None):
+            useri = m[0].get("sender")
+            if useri == None:
                 return "no sender"
-            
-            user_message = userm.get('text')
-            user_id = useri.get('id')
-            if(user_id==None):
+
+            user_message = userm.get("text")
+            user_id = useri.get("id")
+            if user_id == None:
                 return "no sender"
             returnmessage = purdueInfo(user_message)
-            
-            response = createRes(returnmessage,user_id)
-            headers = {'Content-type': 'application/json'}
-            requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + access_token, data=json.dumps(response),headers = headers)
+
+            response = createRes(returnmessage, user_id)
+            headers = {"Content-type": "application/json"}
+            requests.post(
+                "https://graph.facebook.com/v2.6/me/messages/?access_token="
+                + access_token,
+                data=json.dumps(response),
+                headers=headers,
+            )
     return "ok"
 
 
-def createRes(message,userid):
+def createRes(message, userid):
+    """Returns the format the response should be in """
     response = {
-        'messaging_type': 'RESPONSE',
-        'recipient': {'id': userid},
-        'message': {'text': 'hanzen sucks' }
+        "messaging_type": "RESPONSE",
+        "recipient": {"id": userid},
+        "message": {"text": message},
     }
     return response
+
 
 def purdueInfo(message):
     split_string = message.split(" ")
@@ -80,29 +103,38 @@ def purdueInfo(message):
         return mess
     return purdueDining(mess)
 
+
 def purdueDining(text):
-    message =  ""
+    message = ""
     if text == "all":
         for loc in locations:
             message += loc
     else:
-        header={"Accept":"application/json"}
-        res = requests.get(url+text+"/"+time,headers=header).json()
+        header = {"Accept": "application/json"}
+        res = requests.get(URL + text + "/" + time, headers=header).json()
         message = text
         print(res["Location"])
         mealjson = res["Meals"]
         for x in list(range(len(mealjson))):
-            print(mealjson[x]["Name"].encode('ascii', 'ignore'))
+            print(mealjson[x]["Name"].encode("ascii", "ignore"))
             for y in list(range(len(mealjson[x]["Stations"]))):
                 print("-------")
-                print("----"+mealjson[x]["Stations"][y]["Name"].encode('ascii', 'ignore') + "| font=HelveticaNeue-Bold")
+                print(
+                    "----"
+                    + mealjson[x]["Stations"][y]["Name"].encode("ascii", "ignore")
+                    + "| font=HelveticaNeue-Bold"
+                )
                 for z in list(range(len(mealjson[x]["Stations"][y]["Items"]))):
-                    print("----"+mealjson[x]["Stations"][y]["Items"][z]["Name"].encode('ascii', 'ignore'))
+                    print(
+                        "----"
+                        + mealjson[x]["Stations"][y]["Items"][z]["Name"].encode(
+                            "ascii", "ignore"
+                        )
+                    )
                 print(" ")
-        
-        
 
     return message
 
-if __name__=='__main__':
-    app.run(debug=True,use_reloader=True)
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=True)
