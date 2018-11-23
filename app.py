3c9d368a88810f3252e071237e85fde671f10219
Anthony Ko <anthonyko@Anthonys-MacBook-Pro.local>
2018-11-22 22:19:01 -0800

    c

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask, request, jsonify, Response
 import requests
 import json
 import os
+import xml.etree.ElementTree as ET
+import datetime
 app = Flask(__name__)
 url = "https://api.hfs.purdue.edu/menus/v2/locations/"
 locations = ["Wiley", "Ford", "Hillenbrand", "The Gathering Place", "Earhart", "Windsor"]
 verify_token = os.getenv("VERIFY_TOKEN")
+ti1 = datetime.datetime.now()
+time = ti1.strftime("%m-%d-%Y")
 access_token = os.getenv("ACCESS_TOKEN")
-#print(verify_token)
 
 @app.route("/",methods=['GET','POST'])
 def default():
@@ -40,45 +43,66 @@ def webhook():
             user_id = useri.get('id')
             if(user_id==None):
                 return "no sender"
-            loc = getLoc(user_message)
-            if loc == "all":
-                for l in locations:
-                    returnmessage += l + " "
-            elif loc == "none":
-                returnmessage += "none"
-            else:
-                returnmessage += loc + ' '
+            returnmessage = purdueInfo(user_message)
             
             response = createRes(returnmessage,user_id)
-            
             headers = {'Content-type': 'application/json'}
             requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + access_token, data=json.dumps(response),headers = headers)
     return "ok"
-
 
 
 def createRes(message,userid):
     response = {
         'messaging_type': 'RESPONSE',
         'recipient': {'id': userid},
-        'message': {'text': message }
+        'message': {'text': 'hanzen sucks' }
     }
     return response
 
-def getLoc(message):
+def purdueInfo(message):
     split_string = message.split(" ")
+    mess = ""
     if len(split_string) < 4:
-        return "cannot " + message
-    if message.lower() == "list all dining halls".lower():
-        return "all"
+        mess = "none"
+    elif message.lower() == "list all dining halls".lower():
+        mess = "all"
     elif split_string[3] == "today":
         strn = split_string[1]
-        
+        mess = "does not exist"
         for loc in locations:
             if loc == strn:
-                return strn
-        return "no such dining hall"
-    return "cannot " + message
+                mess = loc
+    else:
+        mess = "none"
+    if mess == "none":
+        return "could not understand " + message
+    elif mess == "does not exist":
+        return mess
+    return purdueDining(mess)
+
+def purdueDining(text):
+    message =  ""
+    if text == "all":
+        for loc in locations:
+            message += loc
+    else:
+        header={"Accept":"application/json"}
+        res = requests.get(url+text+"/"+time,headers=header).json()
+        message = text
+        print(res["Location"])
+        mealjson = res["Meals"]
+        for x in list(range(len(mealjson))):
+            print(mealjson[x]["Name"].encode('ascii', 'ignore'))
+            for y in list(range(len(mealjson[x]["Stations"]))):
+                print("-------")
+                print("----"+mealjson[x]["Stations"][y]["Name"].encode('ascii', 'ignore') + "| font=HelveticaNeue-Bold")
+                for z in list(range(len(mealjson[x]["Stations"][y]["Items"]))):
+                    print("----"+mealjson[x]["Stations"][y]["Items"][z]["Name"].encode('ascii', 'ignore'))
+                print(" ")
+        
+        
+
+    return message
 
 if __name__=='__main__':
     app.run(debug=True,use_reloader=True)
